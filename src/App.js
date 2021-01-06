683a26b2ef57dbf6de4337402b5f9b35c2b2111f
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-06 12:54:47 +0700

    try micro-cors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import { vkAuth } from "./config/vkAuth";
+import "./App.css";
 
 function App() {
-
-
-  async function authMe() {
-    await fetch('/api/vk_auth');
+  function authMe() {
+    window.location = `${vkAuth.url}authorize?client_id=${vkAuth.client_id}&redirect_uri=${vkAuth.redirect_uri}`;
   }
+
+  useEffect(() => {
+    const redirectedFromToken = window.location.href.includes("user_id");
+    const redirectedFromAuth = window.location.href.includes("authme");
+    console.log("lol", window.location);
+
+    if (redirectedFromToken) {
+      console.log("редирект с токеном");
+      window.location = "http://inlists.ru";
+    }
+
+    if (redirectedFromAuth) {
+      const code = window.location.href.split("code=")[1];
+      const tokenUrl = `${vkAuth.url}access_token?client_id=${vkAuth.client_id}&redirect_uri=${vkAuth.redirect_uri}&code=${code}&client_secret=${process.env.REACT_APP_VK_SECRET}`;
+      window.location = tokenUrl;
+    }
+  }, [window.location.href]);
 
   return (
     <main>
