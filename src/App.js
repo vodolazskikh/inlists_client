b8703abde8a02ebde0eadc028d6badac62e53268
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-07 20:45:59 +0700

    auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { vkAuth, apiUrl } from "./config";
 import "./App.css";
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("vk_token"));
+  const [userId, setUserId] = useState(localStorage.getItem("vk_user_id"));
+
   function authMe() {
     window.location = `${vkAuth.url}authorize?client_id=${vkAuth.client_id}&redirect_uri=${vkAuth.redirect_uri}`;
   }
-  console.log("process.env.NODE_ENV", process.env);
   useEffect(() => {
     const redirectedFromAuth = window.location.href.includes("authme");
+
+    if (token && !redirectedFromAuth) {
+      return;
+    }
+
     if (redirectedFromAuth) {
       const code = window.location.href.split("code=")[1];
-      fetch(`${apiUrl}/token?code=${code}`).then((data) =>
-        console.log(data.text())
-      );
+      fetch(`${apiUrl}/token?code=${code}`)
+        .then((data) => data.json())
+        .then((tokenAndUserId) => {
+          if (tokenAndUserId.access_token && tokenAndUserId.user_id) {
+            setToken(tokenAndUserId.access_token);
+            setUserId(tokenAndUserId.user_id);
+            localStorage.setItem("vk_token", tokenAndUserId.access_token);
+            localStorage.setItem("vk_user_id", tokenAndUserId.user_id);
+          }
+        });
     }
   }, [window.location.href]);
 
   return (
     <main>
-      <h1>Реакт и сервер на ноде</h1>
+      <h1>Сервис, где ты можешь авторизоваться, чтобы ничего...</h1>
+      {token && <div>{`Твой вк_токен - ${token}`}</div>}
+      {userId && <div>{`Твой юзер_айди - ${userId}`}</div>}
       <button onClick={authMe}>Авторизуй меня</button>
     </main>
   );
--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ export const apiUrl = "https://mighty-refuge-88074.herokuapp.com";
 
 export const vkAuth = {
   url: "https://oauth.vk.com/",
+  methodsApiUrl: "https://api.vk.com/",
   redirect_uri: "http://inlists.ru/authme",
   client_id: "7719105",
 };
