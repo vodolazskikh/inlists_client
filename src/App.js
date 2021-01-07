2d1f88967470f306c0adfb8b9b119f322d632d87
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-07 22:06:02 +0700

    user info

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 function App() {
   const [token, setToken] = useState(localStorage.getItem("vk_token"));
   const [userId, setUserId] = useState(localStorage.getItem("vk_user_id"));
+  const [user, setUser] = useState(undefined);
 
   function authMe() {
     window.location = `${vkAuth.url}authorize?client_id=${vkAuth.client_id}&redirect_uri=${vkAuth.redirect_uri}`;
   }
+
   useEffect(() => {
     const redirectedFromAuth = window.location.href.includes("authme");
 
@@ -18,7 +20,7 @@ function App() {
 
     if (redirectedFromAuth) {
       const code = window.location.href.split("code=")[1];
-      fetch(`${apiUrl}/token?code=${code}`)
+      fetch(`${apiUrl}token?code=${code}`)
         .then((data) => data.json())
         .then((tokenAndUserId) => {
           if (tokenAndUserId.access_token && tokenAndUserId.user_id) {
@@ -31,11 +33,43 @@ function App() {
     }
   }, [window.location.href]);
 
+  useEffect(() => {
+    if (!userId || !token) {
+      return;
+    }
+    fetch(`${apiUrl}userInfo?userId=${userId}&token=${token}`)
+      .then((data) => data.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, [userId]);
+
   return (
     <main>
       <h1>Сервис, где ты можешь авторизоваться, чтобы ничего...</h1>
-      {token && <div>{`Твой вк_токен - ${token}`}</div>}
-      {userId && <div>{`Твой юзер_айди - ${userId}`}</div>}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {user && (
+          <>
+            <div style={{ width: 64, height: 64 }}>
+              <img
+                src={user.photo_100}
+                alt="аватар"
+                height="64px"
+                style={{ borderRadius: "50%" }}
+              />
+            </div>
+            <div>{user.first_name}</div>
+            <div>{user.last_name}</div>
+          </>
+        )}
+      </div>
       <button onClick={authMe}>Авторизуй меня</button>
     </main>
   );
