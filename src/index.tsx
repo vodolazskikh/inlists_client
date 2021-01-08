d8da6f4f7557b81a49f6187b0c27f5bbda31ac0a
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-08 10:03:16 +0700

    migrate from craco to npm-run-all

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { render } from "react-dom";
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import "./index.css";
+import "./tailwind.output.css";
 import reportWebVitals from "./reportWebVitals";
 import { rootReducer } from "state/reducers";
 import thunkMiddleware from "redux-thunk";
